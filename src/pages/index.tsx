0fba5813feddc90453fd08bb487a2bc1a8f6f367
Shayan Shiravani <shayan5420@yahoo.com>
2022-09-22 12:35:55 +0330

    fix: Fix imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
 import Home from '../views/Home'
+import React from 'react'
 
 const HomePage: NextPage = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Home />
   )
 }
 
